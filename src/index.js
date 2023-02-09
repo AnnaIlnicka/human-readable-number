b13a80e35170cf469255743bfab2e41503b8b6d7
Anna Ilnicka <you@example.com>
2023-02-09 22:29:55 +0100

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function toReadable (number) {
-  
+    if (number < 0) {
+
+        console.log("Number Must be greater than zero = " + number);
+        return "";
+    }
+
+    if (number == 0) {
+        console.log("zero");
+        return "zero";
+    }
+   
+        if (number > 100000000000000000000) {
+        console.log("Number is out of range = " + number);
+        return "";
+    }
+    if (!is_numeric(number)) {
+        console.log("Not a number = " + number);
+        return "";
+    }
+ 
+       var Dn = Math.floor(number / 100); /* Tens (deca) */
+    number = number % 100; /* Ones */
+    var tn = Math.floor(number / 10);
+    var one = Math.floor(number % 10);
+    var res = "";
+    
+
+       if (Dn) {
+        res += (((res == "") ? "" : " ") + toReadable(Dn) + " hundred");
+    }
+
+
+    var ones = Array("", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
+    var tens = Array("", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety");
+
+    if (tn > 0 || one > 0) {
+        if (!(res == "")) {
+            res += " ";
+        }
+        if (tn < 2) {
+            res += ones[tn * 10 + one];
+        } else {
+
+            res += tens[tn];
+            if (one > 0) {
+                res += (" " + ones[one]);
+            }
+        }
+    }
+
+    if (res == "") {
+        console.log("Empty = " + number);
+        res = "";
+    }
+    
+    return res;
+}
+function is_numeric(mixed_var) {
+    return (typeof mixed_var === 'number' || typeof mixed_var === 'string') && mixed_var !== '' && !isNaN(mixed_var);
 }
